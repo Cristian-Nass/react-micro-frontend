c13b5e18a5f05659c5b91b79bd0ee54e20b70ab6
Cristian Nass <cristian.tirdad@gmail.com>
2023-10-17 14:27:52 +0200

    created two mf app

---
+++ home/src/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer = () => (
+  <div className='p-5 bg-blue-500 text-white text-3xl font-bold'>
+    Only the best Spinners
+  </div>
+);
+
+export default Footer;
