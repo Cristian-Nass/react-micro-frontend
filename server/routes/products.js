2692825e309dc0bb098c5cf0f9228cdccad6d8d0
Cristian Nass <cristian.tirdad@gmail.com>
2023-10-18 14:12:17 +0200

    get product by Id

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
+const products = require("../products/products");
 
-const post = [
-  { name: "Cris", email: "Cris email" },
-  { name: "Arvid", email: "Arvid email" },
-];
 router.get("/", (req, res) => {
-  res.send(post);
+  res.send(products);
 });
+
+router.get("/:productId", async (req, res) => {
+  const product = products.find((p) => p.id === +req.params.productId);
+  res.send(product);
+});
+
 module.exports = router;
